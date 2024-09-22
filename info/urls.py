a945f4d0678b5ed70630aabbfef23a9197be9506
rzadrbi <>
2024-09-22 22:53:40 +0330

    Initial commit

---
+++ info/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+app_name = 'info'
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name="index.html"), name='main'),
+]
